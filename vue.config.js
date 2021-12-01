379b6f10960d999eab8161740a89aee6e0970416
Deepak <deepaksharmawins@gmail.com>
2021-12-01 13:44:01 +0530

    colspan and link on name field

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,20 @@
-module.exports={
+const webpack = require('webpack')
+module.exports = {
+  //Setting here public path for deployment
+  publicPath: process.env.NODE_ENV === 'production'? '': '/',
+  devServer: {
+    port: 8081,
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1
+      })
+    ],
+  },
+  chainWebpack:
+    config => {
+      config.optimization.delete('splitChunks')
+    },
+  filenameHashing: false,
 }
