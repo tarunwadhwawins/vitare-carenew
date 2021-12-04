716911990c16e7c83379adbe8ab116d6f7c9c66d
Pawanjeet Kaur <pawanjeet.kaur@ditstek.com>
2021-12-03 18:42:42 -0600

    card

--- src/components/common/cards/LongCard.vue
+++ src/components/common/cards/LongCard.vue
@@ -30,35 +30,3 @@ export default {
   },
 }
 </script>
-
-<style>
-.colorBox {
-  padding: 15px;
-  border-radius: 10px;
-}
-.colorBox p {
-  font-size: 16px;
-  display: block;
-  color: #fff;
-}
-.anticon svg {
-  display: inline-block;
-  color: #ffffff;
-}
-.colorBox .anticon svg {
-  fill: #ffffff !important;
-}
-.colorBox .anticon {
-  font-size: 40px;
-  margin: 0 0 8px 0;
-}
-.colorBox h3 {
-  position: absolute;
-  right: 15px;
-  top: 50%;
-  margin: 0;
-  font-size: 50px;
-  transform: translatey(-50%);
-  color: #fff;
-}
-</style>
