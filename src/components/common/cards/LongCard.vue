17bae4ae53782de7be553f1e96d881022075e0e8
manoj <manoj.yadav@ditstek.com>
2022-01-25 16:36:57 +0530

    worked on patients list and staff list table

--- src/components/common/cards/LongCard.vue
+++ src/components/common/cards/LongCard.vue
@@ -1,36 +1,34 @@
 <template>
-<div v-bind:style="{ backgroundColor: backgroundColor, color: textColor }" class="colorBox">
-    <UserOutlined />
-    <h3>{{ count }}</h3>
-    <p>{{ text }}</p>
-</div>
+    <div v-bind:style="{ backgroundColor: backgroundColor, color: textColor }" class="colorBox">
+      <UserOutlined />
+      <h3>{{ count }}</h3>
+      <p>{{ text }}</p>
+    </div>
 </template>
 
 <script>
-import {
-    UserOutlined
-} from "@ant-design/icons-vue";
+import { UserOutlined } from "@ant-design/icons-vue";
 export default {
-    components: {
-        UserOutlined
+  components: {
+    UserOutlined
+  },
+  props: {
+    backgroundColor: {
+      type: String,
+      required: true
     },
-    props: {
-        backgroundColor: {
-            type: String,
-            required: true
-        },
-        textColor: {
-            type: String,
-            required: true
-        },
-        count: {
-            type: Number,
-            required: true
-        },
-        text: {
-            type: String,
-            required: true
-        },
+    textColor: {
+      type: String,
+      required: true
     },
+    count: {
+      type: Number,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+  },
 }
 </script>
