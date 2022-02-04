531aa9c0ee6aba88b127e6231c702da2908f93d9
Pawanjeet Kaur <pawanjeet.kaur@ditstek.com>
2022-02-04 17:18:21 -0600

    Delete Inventory

--- src/store/inventory/mutations.js
+++ src/store/inventory/mutations.js
@@ -15,3 +15,7 @@ export const inventoryDetailsSuccess = async (state, inventories) => {
 export const updateInventorySuccess = async (state, inventories) => {
   state.updateInventory = inventories;
 }
+
+export const deleteInventorySuccess = async (state, inventories) => {
+  state.deleteInventory = inventories;
+}
