d3c16a6e08fa308fb7cebc371e8ac00cb9c024fd
Pawanjeet Kaur <pawanjeet.kaur@ditstek.com>
2022-02-04 15:14:51 -0600

    hidden-dropdown

--- src/components/common/input/SearchField.vue
+++ src/components/common/input/SearchField.vue
@@ -1,3 +1,7 @@
 <template>
-  <a-select mode="tags" style="width: 100%" placeholder="Search..." />
+  <a-select mode="tags" style="width: 100%" placeholder="Search..." dropdownClassName="hidden-dropdown" />
 </template>
+
+<style>
+  .hidden-dropdown { display: none; }
+</style>
