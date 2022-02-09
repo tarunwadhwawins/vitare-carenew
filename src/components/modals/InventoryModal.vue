069a4dbb332394c3a4b6d8abb583940ff93101c8
Pawanjeet Kaur <pawanjeet.kaur@ditstek.com>
2022-02-09 11:06:51 -0600

    Inventory Add & Lisitng

--- src/components/modals/InventoryModal.vue
+++ src/components/modals/InventoryModal.vue
@@ -126,11 +126,11 @@ export default {
       }
     })
 
-    const deviceModelId = ref(null);
+    const modelId = ref(null);
     const onSelectModal = (selected) => {
       deviceModals.value.forEach(modal => {
         if(modal.value == selected) {
-          deviceModelId.value = modal.id;
+          modelId.value = modal.id;
         }
       });
     };
@@ -151,7 +151,7 @@ export default {
         emit('is-visible', false);
       }
       else {
-        inventoryForm.deviceModelId = deviceModelId.value;
+        inventoryForm.deviceModelId = modelId.value;
         store.dispatch('addInventory', inventoryForm).then(() => {
           store.dispatch('inventoriesList')
         })
