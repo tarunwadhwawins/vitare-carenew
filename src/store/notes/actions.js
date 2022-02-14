910876828b6cb201a2247cf4dd977885969cab72
Pawanjeet Kaur <pawanjeet.kaur@ditstek.com>
2022-02-14 11:32:50 -0600

    Notes Listing

--- src/store/notes/actions.js
+++ src/store/notes/actions.js
@@ -25,29 +25,3 @@ export const notesList = async ({ commit }) => {
 		commit('failure', error.response.data);
 	})
 }
-
-export const deleteNote = async ({ commit }, id) => {
-  console.log('id', id)
-	await ServiceMethodService.common("delete", API_ENDPOINTS['notes'], id, null).then((response) => {
-		commit('deleteNoteSuccess', response.data.data);
-	})
-	.catch((error) => {
-		if (error.response.status == 401) {
-			//AuthService.logout();
-		}
-		commit('failure', error.response.data);
-	})
-}
-
-export const deviceModalsList = async ({ commit }, id) => {
-  console.log('id', id)
-	await ServiceMethodService.common("get", API_ENDPOINTS['model']+'?deviceType='+id, null, null).then((response) => {
-		commit('deviceModalsListSuccess', response.data.data);
-	})
-	.catch((error) => {
-		if (error.response.status == 401) {
-			//AuthService.logout();
-		}
-		commit('failure', error.response.data);
-	})
-}
--- src/store/notes/state.js
+++ src/store/notes/state.js
@@ -1,5 +1,4 @@
 export default {
   addNote: null,
   notesList: null,
-  deleteNote: null,
 }
