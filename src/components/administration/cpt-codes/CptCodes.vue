4983df11f39be5358b174b4d6d4fa395152d9ce7
Harkeemat Singh <harkeemat.singh@ditstek.com>
2022-03-03 13:22:10 +0530

    formate code

--- src/components/administration/cpt-codes/CptCodes.vue
+++ src/components/administration/cpt-codes/CptCodes.vue
@@ -1,41 +1,40 @@
 <template>
-  <a-layout>
+<a-layout>
     <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
-      <Header />
+        <Header />
     </a-layout-header>
     <a-layout>
-      <Sidebar />
-      <a-layout-content>
-        <div class="common-bg">
-          <a-row>
-            <a-col :span="24">
-              <h2 class="pageTittle">
-                CPT Codes
-                <div class="commonBtn">
-                  <Button :name="buttonName" @click="showModal(true)" />
-                </div>
-              </h2>
-            </a-col>
-            <a-col :span="12">
-              <SearchField @change="searchData"/>
-            </a-col>
-            <a-col :span="12">
-              <div class="text-right mb-24">
-                <a-button class="primaryBtn">Export to Excel</a-button>
-              </div>
-            </a-col>
-            <a-col :span="24">
-              <CptCodesTable v-if="cptCodesList.cptCodesList" :cptCodesList="cptCodesList" />
-            </a-col>
-          </a-row>
-        </div>
-      </a-layout-content>
+        <Sidebar />
+        <a-layout-content>
+            <div class="common-bg">
+                <a-row>
+                    <a-col :span="24">
+                        <h2 class="pageTittle">
+                            CPT Codes
+                            <div class="commonBtn">
+                                <Button :name="buttonName" @click="showModal(true)" />
+                            </div>
+                        </h2>
+                    </a-col>
+                    <a-col :span="12">
+                        <SearchField @change="searchData" />
+                    </a-col>
+                    <a-col :span="12">
+                        <div class="text-right mb-24">
+                            <a-button class="primaryBtn">Export to Excel</a-button>
+                        </div>
+                    </a-col>
+                    <a-col :span="24">
+                        <CptCodesTable v-if="cptCodesList.cptCodesList" :cptCodesList="cptCodesList" />
+                    </a-col>
+                </a-row>
+            </div>
+        </a-layout-content>
     </a-layout>
-  </a-layout>
+</a-layout>
 
-  <!-- Add CPT Code Modal -->
-  <CptCodesModal v-model:visible="visible" @ok="handleOk" @is-visible="showModal($event)" />
-
+<!-- Add CPT Code Modal -->
+<CptCodesModal v-model:visible="visible" @ok="handleOk" @is-visible="showModal($event)" />
 </template>
 
 <script>
@@ -43,98 +42,103 @@ import Header from "@/components/administration/layout/header/Header";
 import Sidebar from "@/components/administration/layout/sidebar/Sidebar";
 import CptCodesModal from "@/components/modals/CptCodesModal";
 import CptCodesTable from "@/components/administration/cpt-codes/tables/CptCodesTable";
-import { ref,watchEffect } from "vue";
+import {
+    ref,
+    watchEffect
+} from "vue";
 import SearchField from "@/components/common/input/SearchField";
 import Button from "@/components/common/button/Button";
-import { useStore } from "vuex"
+import {
+    useStore
+} from "vuex"
 export default {
-  components: {
-    Header,
-    Sidebar,
-    CptCodesModal,
-    CptCodesTable,
-    SearchField,
-    Button,
-  },
-  setup() {
-    const visible = ref(false);
-    const showModal = (e) => {
-      visible.value = e;
-    };
-    const store = useStore()
-    const handleOk = () => {
-      
-      visible.value = false;
-    };
+    components: {
+        Header,
+        Sidebar,
+        CptCodesModal,
+        CptCodesTable,
+        SearchField,
+        Button,
+    },
+    setup() {
+        const visible = ref(false);
+        const showModal = (e) => {
+            visible.value = e;
+        };
+        const store = useStore()
+        const handleOk = () => {
 
-    const searchData = (value) => {
-      console.log('searchGlobalCodes', value)
-    };
-    watchEffect(()=>{
-      store.dispatch('serviceList')
-      store.dispatch('cptCodesList')
+            visible.value = false;
+        };
 
-    })
-    const cptCodesList = store.getters.cptRecords.value
-    // const cptCodesList = [
-    //   {
-    //     key: 1,
-    //     cpt: "90791",
-    //     description: "Evaluation",
-    //     billing: "$150",
-    //     active: "",
-    //     action: "",
-    //   },
-    //   {
-    //     key: 2,
-    //     cpt: "90832",
-    //     description: "LCSW Ongoing Services",
-    //     billing: "$200",
-    //     active: "",
-    //     action: "",
-    //   },
-    //   {
-    //     key: 3,
-    //     cpt: "96130",
-    //     description: "Deep Dive",
-    //     billing: "$120",
-    //     active: "",
-    //     action: "",
-    //   },
-    //   {
-    //     key: 4,
-    //     cpt: "96138",
-    //     description: "M3 Screening - MSW",
-    //     billing: "$160",
-    //     active: "",
-    //     action: "",
-    //   },
-    //   {
-    //     key: 5,
-    //     cpt: "99358",
-    //     description: "Patient Discharge",
-    //     billing: "$300",
-    //     active: "",
-    //     action: "",
-    //   },
-    //   {
-    //     key: 6,
-    //     cpt: "99453",
-    //     description: "(ES Team) Setup and patient education for use of RPM equipment",
-    //     billing: "$150",
-    //     active: "",
-    //     action: "",
-    //   },
-    // ];
+        const searchData = (value) => {
+            console.log('searchGlobalCodes', value)
+        };
+        watchEffect(() => {
+            store.dispatch('serviceList')
+            store.dispatch('cptCodesList')
 
-    return {
-      cptCodesList,
-      searchData,
-      visible,
-      showModal,
-      handleOk,
-      buttonName: 'Add CPT Code',
-    };
-  },
+        })
+        const cptCodesList = store.getters.cptRecords.value
+        // const cptCodesList = [
+        //   {
+        //     key: 1,
+        //     cpt: "90791",
+        //     description: "Evaluation",
+        //     billing: "$150",
+        //     active: "",
+        //     action: "",
+        //   },
+        //   {
+        //     key: 2,
+        //     cpt: "90832",
+        //     description: "LCSW Ongoing Services",
+        //     billing: "$200",
+        //     active: "",
+        //     action: "",
+        //   },
+        //   {
+        //     key: 3,
+        //     cpt: "96130",
+        //     description: "Deep Dive",
+        //     billing: "$120",
+        //     active: "",
+        //     action: "",
+        //   },
+        //   {
+        //     key: 4,
+        //     cpt: "96138",
+        //     description: "M3 Screening - MSW",
+        //     billing: "$160",
+        //     active: "",
+        //     action: "",
+        //   },
+        //   {
+        //     key: 5,
+        //     cpt: "99358",
+        //     description: "Patient Discharge",
+        //     billing: "$300",
+        //     active: "",
+        //     action: "",
+        //   },
+        //   {
+        //     key: 6,
+        //     cpt: "99453",
+        //     description: "(ES Team) Setup and patient education for use of RPM equipment",
+        //     billing: "$150",
+        //     active: "",
+        //     action: "",
+        //   },
+        // ];
+
+        return {
+            cptCodesList,
+            searchData,
+            visible,
+            showModal,
+            handleOk,
+            buttonName: 'Add CPT Code',
+        };
+    },
 };
 </script>
--- src/components/administration/cpt-codes/tables/CptCodesTable.vue
+++ src/components/administration/cpt-codes/tables/CptCodesTable.vue
@@ -1,105 +1,115 @@
 <template>
-  <a-table  rowKey="id"
-    :columns="cptCodesColumns"
-    :data-source="data"
-    :scroll="{ y: 400 }"
-    :pagination="false"
-    @change="onChange">
-    <template #actions="{record}" >
-      <a-tooltip placement="bottom">
-        <template #title>
-          <span>Edit</span>
-        </template>
-        <a class="icons"><EditOutlined /></a>
-      </a-tooltip>
-      <a-tooltip placement="bottom" @click="deleteCpt(record.udid)">
-        <template #title>
-            <span>{{$t('global.delete')}}</span>
-        </template>
-        <a class="icons">
-            <DeleteOutlined /></a>
-    </a-tooltip>
+<a-table rowKey="id" :columns="cptCodesColumns" :data-source="data" :scroll="{ y: 400 }" :pagination="false" @change="onChange">
+    <template #actions="{record}">
+        <a-tooltip placement="bottom">
+            <template #title>
+                <span>Edit</span>
+            </template>
+            <a class="icons">
+                <EditOutlined /></a>
+        </a-tooltip>
+        <a-tooltip placement="bottom" @click="deleteCpt(record.udid)">
+            <template #title>
+                <span>{{$t('global.delete')}}</span>
+            </template>
+            <a class="icons">
+                <DeleteOutlined /></a>
+        </a-tooltip>
     </template>
     <template #status="{record}">
-      <a-switch v-model:checked="record.status" />
+        <a-switch v-model:checked="record.status" />
     </template>
-  </a-table>
-  <InfiniteLoader v-if="loader" />
+</a-table>
+<InfiniteLoader v-if="loader" />
 </template>
 
 <script>
-import { ref, reactive, onMounted } from "vue";
-import { DeleteOutlined, EditOutlined } from "@ant-design/icons-vue";
-import { messages } from "@/config/messages";
-import InfiniteLoader from "@/components/loader/InfiniteLoader";
-import { useStore } from "vuex";
-import { warningSwal } from "@/commonMethods/commonMethod";
-export default {
-  components: {
+import {
+    ref,
+    reactive,
+    onMounted
+} from "vue";
+import {
     DeleteOutlined,
-    EditOutlined,
-    InfiniteLoader
-  },
-  props: {
-    cptCodesList: {
-      type: Array
-    }
-  },
-  setup(props) {
-     const cptCodesColumns = reactive(props.cptCodesList.cptCodesColumns)
-     const data = reactive(props.cptCodesList.cptCodesList)
-     const store = useStore()
-     function deleteCpt(id) {
-       console.log(id)
-      warningSwal(messages.deleteWarning).then((response) => {
-        if (response == true) {
-          store.dispatch("deleteCptCode", id).then(()=>{
-            store.state.cptCodes.cptCodesList = ""
-            store.dispatch('cptCodesList')
-          })
+    EditOutlined
+} from "@ant-design/icons-vue";
+import {
+    messages
+} from "@/config/messages";
+import InfiniteLoader from "@/components/loader/InfiniteLoader";
+import {
+    useStore
+} from "vuex";
+import {
+    warningSwal
+} from "@/commonMethods/commonMethod";
+export default {
+    components: {
+        DeleteOutlined,
+        EditOutlined,
+        InfiniteLoader
+    },
+    props: {
+        cptCodesList: {
+            type: Array
         }
-      });
-    }
+    },
+    setup(props) {
+        const cptCodesColumns = reactive(props.cptCodesList.cptCodesColumns)
+        const data = reactive(props.cptCodesList.cptCodesList)
+        const store = useStore()
 
-    //infinite scroll
-    const meta = store.getters.timeLogReports;
-    const loader = ref(false);
-    onMounted(() => {
-      var tableContent = document.querySelector(".ant-table-body");
-      tableContent.addEventListener("scroll", (event) => {
-        let maxScroll = event.target.scrollHeight - event.target.clientHeight;
-        let currentScroll = event.target.scrollTop + 2;
-        if (currentScroll >= maxScroll) {
-          let current_page = meta.cptMeta.current_page + 1;
-
-          if (current_page <= meta.cptMeta.total_pages) {
-            loader.value = true;
-            meta.cptMeta = "";
-            store.state.cptCodes.cptCodesList = ""
-            store.dispatch("cptCodesList", "?page=" + current_page).then(()=>{
-              loadMoredata();
-            })
-
-          }
+        function deleteCpt(id) {
+            console.log(id)
+            warningSwal(messages.deleteWarning).then((response) => {
+                if (response == true) {
+                    store.dispatch("deleteCptCode", id).then(() => {
+                        store.state.cptCodes.cptCodesList = ""
+                        store.dispatch('cptCodesList')
+                    })
+                }
+            });
         }
-      });
-    });
 
-    function loadMoredata() {
-      const newData = meta.cptCodesList;
-     
-      newData.forEach((element) => {
-        data.push(element);
-      });
-      loader.value = false;
-    }
+        //infinite scroll
+        const meta = store.getters.timeLogReports;
+        const loader = ref(false);
+        onMounted(() => {
+            var tableContent = document.querySelector(".ant-table-body");
+            tableContent.addEventListener("scroll", (event) => {
+                let maxScroll = event.target.scrollHeight - event.target.clientHeight;
+                let currentScroll = event.target.scrollTop + 2;
+                if (currentScroll >= maxScroll) {
+                    let current_page = meta.cptMeta.current_page + 1;
 
-    return {
-      cptCodesColumns,
-      data,
-      loader,
-      deleteCpt,
+                    if (current_page <= meta.cptMeta.total_pages) {
+                        loader.value = true;
+                        meta.cptMeta = "";
+                        store.state.cptCodes.cptCodesList = ""
+                        store.dispatch("cptCodesList", "?page=" + current_page).then(() => {
+                            loadMoredata();
+                        })
+
+                    }
+                }
+            });
+        });
+
+        function loadMoredata() {
+            const newData = meta.cptCodesList;
+
+            newData.forEach((element) => {
+                data.push(element);
+            });
+            loader.value = false;
+        }
+
+        return {
+            cptCodesColumns,
+            data,
+            loader,
+            deleteCpt,
+        }
     }
-  }
 }
 </script>
