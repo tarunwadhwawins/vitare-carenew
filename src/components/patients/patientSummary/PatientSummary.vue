94ffbb0719d8f6dc7524e4495ea1b5fcddcdbbdd
Pawanjeet Kaur <pawanjeet.kaur@ditstek.com>
2022-03-07 16:26:28 -0600

    Time Logs

--- src/components/patients/patientSummary/PatientSummary.vue
+++ src/components/patients/patientSummary/PatientSummary.vue
@@ -181,6 +181,7 @@ export default {
       store.dispatch('patientVitals', {patientId: route.params.udid, deviceType: 100});
       store.dispatch('patientVitals', {patientId: route.params.udid, deviceType: 101});
       store.dispatch('devices', route.params.udid)
+      store.dispatch('cptCodesList')
       timer.value = setInterval(() => {
         elapsedTime.value += 1000;
       }, 1000);
