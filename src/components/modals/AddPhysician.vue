2f72b0b7bbc9181e55490f13b65d56f163b3ea6d
harkeemat-bhullar <harkeemat.singh@ditstek.com>
2022-04-01 12:44:39 +0530

    Fixed issue staff and patients select box

--- src/components/modals/AddPhysician.vue
+++ src/components/modals/AddPhysician.vue
@@ -1,17 +1,12 @@
-
 <template>
-<a-modal title="Add New Physician" :maskClosable="maskebale" @cancel="closeModal()">
+<a-modal title="Add New Care Coordinator" :maskClosable="maskebale" @cancel="closeModal()">
     <a-form ref="formRef" :model="physicianForm" layout="vertical" @finish="addPhysician" @finishFailed="onFinishFailed">
         <a-row :gutter="24">
             <a-col :sm="24" :xs="24">
                 <div class="form-group">
-                    <a-form-item :label="$t('appointmentCalendar.physician')" name="staffId" :rules="[{ required: true, message: $t('appointmentCalendar.physician')+' '+$t('global.validation')  }]"> 
-                        <!-- <a-select ref="select" v-if="staffList.allStaffList" v-model:value="physicianForm.staffId" style="width: 100%" size="large">
-                            <a-select-option value="" hidden>{{'Select Staff'}}</a-select-option>
-                            <a-select-option v-for="staff in staffList.allStaffList" :key="staff.id" :value="staff.id">{{ staff.fullName }}
-                            </a-select-option>
-                        </a-select> -->
-                        <StaffDropDown v-if="staffList.allStaffList"  v-model:value="physicianForm.staffId" @handleStaffChange="handleStaffChange($event)"/>
+                    <a-form-item :label="$t('appointmentCalendar.physician')" name="staffId" :rules="[{ required: true, message: $t('appointmentCalendar.physician')+' '+$t('global.validation')  }]">
+
+                        <StaffDropDown v-if="staffList.allStaffList" v-model:value="physicianForm.staffId" @handleStaffChange="handleStaffChange($event)" :close="closeValue" />
                     </a-form-item>
                 </div>
             </a-col>
@@ -26,101 +21,115 @@
     </a-form>
 </a-modal>
 </template>
+
 <script>
-import { ref, reactive } from "vue";
-import { useStore } from "vuex";
-import { arrayToObjact } from "@/commonMethods/commonMethod";
+import {
+    ref,
+    reactive
+} from "vue";
+import {
+    useStore
+} from "vuex";
+import {
+    arrayToObjact
+} from "@/commonMethods/commonMethod";
 import StaffDropDown from "@/components/modals/search/StaffDropdownSearch.vue";
 export default {
-  props: {},
-  components:{
-      StaffDropDown,
-  },
-  setup(props, { emit }) {
-    const options = ref([
-      {
-        value: "Steve Smith",
-        label: "Steve Smith",
-      },
-      {
-        value: "Robert Henry",
-        label: "Robert Henry",
-      },
-      {
-        value: "Smith Joseph",
-        label: "Smith Joseph",
-      },
-      {
-        value: "Jane Doe",
-        label: "Jane Doe",
-      },
-    ]);
-    const store = useStore();
-    const handleChange = (value) => {
-      console.log(`selected ${value}`);
-    };
+    props: {},
+    components: {
+        StaffDropDown,
+    },
+    setup(props, {
+        emit
+    }) {
+        const closeValue = ref(false)
+        const options = ref([{
+                value: "Steve Smith",
+                label: "Steve Smith",
+            },
+            {
+                value: "Robert Henry",
+                label: "Robert Henry",
+            },
+            {
+                value: "Smith Joseph",
+                label: "Smith Joseph",
+            },
+            {
+                value: "Jane Doe",
+                label: "Jane Doe",
+            },
+        ]);
+        const store = useStore();
+        const handleChange = (value) => {
+            console.log(`selected ${value}`);
+        };
 
-    const handleBlur = () => {
-    };
+        const handleBlur = () => {};
 
-    const handleFocus = () => {
-    };
+        const handleFocus = () => {};
 
-    const filterOption = (input, option) => {
-      return option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0;
-    };
-    const staffList = store.getters.commonRecords.value;
+        const filterOption = (input, option) => {
+            return option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+        };
+        const staffList = store.getters.commonRecords.value;
 
-    const physicianForm = reactive({
-      staffId: "",
-    });
-    const addPhysician = () => {
-      store.dispatch("staffSummary", physicianForm.staffId).then(() => {
-        addStaff(physicianForm.staffId);
-      });
-    };
-    const form = reactive({
-      ...physicianForm,
-    });
+        const physicianForm = reactive({
+            staffId: "",
+        });
+        const addPhysician = () => {
+            store.dispatch("staffSummary", physicianForm.staffId).then(() => {
+                addStaff(physicianForm.staffId);
+            });
+        };
+        const form = reactive({
+            ...physicianForm,
+        });
 
-    function addStaff(event) {
-      Object.assign(physicianForm, form);
-      //console.log(store.getters.appointmentRecords.value.getStaff)
-      let objact = arrayToObjact(
-        store.getters.appointmentRecords.value.getStaff,
-        event
-      );
-      if (!objact) {
-        store.dispatch(
-          "getStaffs",
-          store.state.careCoordinatorSummary.staffSummary
-        );
-      }
+        function addStaff(event) {
+            Object.assign(physicianForm, form);
+            //console.log(store.getters.appointmentRecords.value.getStaff)
+            let objact = arrayToObjact(
+                store.getters.appointmentRecords.value.getStaff,
+                event
+            );
+            if (!objact) {
+                store.dispatch(
+                    "getStaffs",
+                    store.state.careCoordinatorSummary.staffSummary
+                );
+            }
 
-      emit("is-visible", false);
-    }
-    function closeModal() {
-      Object.assign(physicianForm, form);
-      emit("is-visible", false);
-    }
+            emit("is-visible", false);
+        }
 
-    const handleStaffChange = (val) => {
-      physicianForm.staffId = val;
-    };
+        function closeModal() {
+            closeValue.value = true
+            Object.assign(physicianForm, form);
+            setTimeout(() => {
+                closeValue.value = false
+            }, 100)
+            emit("is-visible", false);
+        }
 
-    return {
-      handleStaffChange,
-      value: ref(undefined),
-      filterOption,
-      handleBlur,
-      handleFocus,
-      handleChange,
-      options,
-      staffList,
-      physicianForm,
-      addPhysician,
-      closeModal,
-    };
-  },
+        const handleStaffChange = (val) => {
+            physicianForm.staffId = val;
+        };
+
+        return {
+            handleStaffChange,
+            value: ref(undefined),
+            filterOption,
+            handleBlur,
+            handleFocus,
+            handleChange,
+            options,
+            staffList,
+            physicianForm,
+            addPhysician,
+            closeModal,
+            closeValue
+        };
+    },
 };
 </script>
