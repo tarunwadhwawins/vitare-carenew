2133c774967dd37d3046a535bf5d93c463e4bdba
harkeemat-bhullar <harkeemat.singh@ditstek.com>
2022-04-09 14:34:49 +0530

    fixed issue in threshold

--- src/components/modals/Thresholds.vue
+++ src/components/modals/Thresholds.vue
@@ -21,7 +21,7 @@
                             </a-select-option>
                             <a-select-option v-for="device in globalCode.deviceType.globalCode" :key="device.id" :value="device.id">{{ device.name }}</a-select-option>
                         </a-select> -->
-                        <GlobalCodeDropDown v-model:value="thresholdForm.deviceTypeId" :globalCode="globalCode.deviceType" :disabled="threshodId ? true : false" @change="handleDevice"/>
+                        <GlobalCodeDropDown v-model:value="thresholdForm.deviceTypeId" :globalCode="globalCode.deviceType" :disabled="threshodId ? true : false" @change="handleDevice" />
                         <ErrorMessage v-if="errorMsg" :name="errorMsg.deviceType ? errorMsg.deviceType[0] : ''" />
                     </a-form-item>
                 </div>
@@ -62,174 +62,191 @@
 </template>
 
 <script>
-import { ref, reactive, computed, watchEffect } from "vue";
+import {
+    ref,
+    reactive,
+    computed,
+    watchEffect
+} from "vue";
 import ModalButtons from "@/components/common/button/ModalButtons";
 import ErrorMessage from "../common/messages/ErrorMessage";
-import { useStore } from "vuex";
-import { regex } from "@/RegularExpressions/regex";
+import {
+    useStore
+} from "vuex";
+import {
+    regex
+} from "@/RegularExpressions/regex";
 import Loader from "@/components/loader/Loader";
-import { warningSwal } from "@/commonMethods/commonMethod";
-import { messages } from "../../config/messages";
+import {
+    warningSwal
+} from "@/commonMethods/commonMethod";
+import {
+    messages
+} from "../../config/messages";
 import GlobalCodeDropDown from "@/components/modals/search/GlobalCodeSearch.vue";
 
 const OPTIONS = ["Jane Doe", "Steve Smith", "Joseph William"];
 const OPTIONSTAG = ["Admin", "Clinical", "Office", "Personal"];
 
 export default {
-  components: {
-    ErrorMessage,
-    ModalButtons,
-    Loader,
-    GlobalCodeDropDown,
-  },
-  props: {
-    threshodId: {
-      type: String,
+    components: {
+        ErrorMessage,
+        ModalButtons,
+        Loader,
+        GlobalCodeDropDown,
     },
-  },
-  setup(props, { emit }) {
-    const store = useStore();
+    props: {
+        threshodId: {
+            type: String,
+        },
+    },
+    setup(props, {
+        emit
+    }) {
+        const store = useStore();
 
-    const value = ref();
-    const value2 = ref();
-    const formRef = ref();
-    const formButton = ref(false);
-    const selectedItems = ref(["Jane Doe"]);
-    const filteredOptions = computed(() =>
-      OPTIONS.filter((o) => !selectedItems.value.includes(o))
-    );
+        const value = ref();
+        const value2 = ref();
+        const formRef = ref();
+        const formButton = ref(false);
+        const selectedItems = ref(["Jane Doe"]);
+        const filteredOptions = computed(() =>
+            OPTIONS.filter((o) => !selectedItems.value.includes(o))
+        );
 
-    const selectedItemsForTag = ref(["Admin"]);
-    const filteredOptionsForTag = computed(() =>
-      OPTIONSTAG.filter((o) => !selectedItemsForTag.value.includes(o))
-    );
+        const selectedItemsForTag = ref(["Admin"]);
+        const filteredOptionsForTag = computed(() =>
+            OPTIONSTAG.filter((o) => !selectedItemsForTag.value.includes(o))
+        );
 
-    const thresholdForm = reactive({
-      deviceType: "",
-      deviceTypeId: "",
-      generalParametersGroup: "",
-      generalParametershigh: [],
-      generalParameterslow: [],
-      udid: [],
-    });
-    const form = reactive({
-      ...thresholdForm,
-    });
-    const globalCode = computed(() => {
-      return store.state.common;
-    });
-
-    const vitalData = computed(() => {
-      return store.state.thresholds.vitalData;
-    });
-    const vitalEdit = computed(() => {
-      return store.state.thresholds;
-    });
-
-    function closeModal() {
-      console.log("check", thresholdForm);
-      if (
-        thresholdForm.generalParametersGroup != "" ||
-        thresholdForm.deviceTypeId != ""
-      ) {
-        warningSwal(messages.modalWarning).then((response) => {
-          if (response == true) {
-            ///console.log("check2")
-            handleCancel();
-            emit("is-visible", false);
-          } else {
-            emit("is-visible", true);
-          }
+        const thresholdForm = reactive({
+            deviceType: "",
+            deviceTypeId: "",
+            generalParametersGroup: "",
+            generalParametershigh: [],
+            generalParameterslow: [],
+            udid: [],
         });
-      }
-    }
+        const form = reactive({
+            ...thresholdForm,
+        });
+        const globalCode = computed(() => {
+            return store.state.common;
+        });
 
-    function handleDevice() {
-      store.state.thresholds.vitalData = null;
-      thresholdForm.generalParametershigh = [];
-      thresholdForm.generalParameterslow = [];
-      store.dispatch("getVital", thresholdForm.deviceTypeId);
-    }
+        const vitalData = computed(() => {
+            return store.state.thresholds.vitalData;
+        });
+        const vitalEdit = computed(() => {
+            return store.state.thresholds;
+        });
 
-    const submitForm = () => {
-      formButton.value = true;
-      let parameter = [];
-      thresholdForm.generalParameterslow.forEach(function (Element, i) {
-        if (Element) {
-          console.log("Element", Element);
-          parameter.push({
-            type: i,
-            lowLimit: Element,
-            highLimit: thresholdForm.generalParametershigh[i],
-            parameterId: props.threshodId ? thresholdForm.udid[i] : "",
-          });
+        function closeModal() {
+            console.log("check", thresholdForm);
+            if (
+                thresholdForm.generalParametersGroup != "" ||
+                thresholdForm.deviceTypeId != ""
+            ) {
+                warningSwal(messages.modalWarning).then((response) => {
+                    if (response == true) {
+                        ///console.log("check2")
+                        handleCancel();
+                        emit("is-visible", false);
+                    } else {
+                        emit("is-visible", true);
+                    }
+                });
+            }
         }
-      });
-      if (props.threshodId != null) {
-        store.dispatch("updateGeneralParameterGroup", {
-          data: {
-            deviceTypeId: thresholdForm.deviceTypeId,
-            generalParameterGroup: thresholdForm.generalParametersGroup,
-            parameter: parameter,
-          },
-          id: props.threshodId,
-        });
-      } else {
-        store.dispatch("addGeneralParameterGroup", {
-          deviceTypeId: thresholdForm.deviceTypeId,
-          generalParameterGroup: thresholdForm.generalParametersGroup,
-          parameter: parameter,
-        });
-      }
 
-      setTimeout(() => {
-        store.getters.vitalDataGetters.value.vitalList = "";
-        store.dispatch("generalParameterList");
-        emit("is-visible", false);
-        handleCancel();
-      }, 2000);
-    };
-
-    const handleCancel = () => {
-      formRef.value.resetFields();
-      Object.assign(thresholdForm, form);
-      store.commit("vitalNull");
-    };
-    watchEffect(() => {
-      if (props.threshodId != null) {
-        if (vitalEdit.value.vitalEdit) {
-          store.commit("loadingStatus", true);
-          store.dispatch("getVital", vitalEdit.value.vitalEdit.deviceTypeId);
-
-          Object.assign(thresholdForm, vitalEdit.value.vitalEdit);
-
-          setTimeout(() => {
-            store.commit("loadingStatus", false);
-          }, 2000);
+        function handleDevice() {
+            store.state.thresholds.vitalData = null;
+            thresholdForm.generalParametershigh = [];
+            thresholdForm.generalParameterslow = [];
+            store.dispatch("getVital", thresholdForm.deviceTypeId);
         }
-      }
-    });
 
-    return {
-      handleDevice,
-      globalCode,
-      thresholdForm,
-      vitalData,
-      handleCancel,
-      submitForm,
-      selectedItems,
-      filteredOptions,
-      filteredOptionsForTag,
-      selectedItemsForTag,
-      size: ref("large"),
-      value,
-      value2,
-      formRef,
-      regex,
-      vitalEdit,
-      closeModal,
-      formButton,
-    };
-  },
+        const submitForm = () => {
+            formButton.value = true;
+            let parameter = [];
+            thresholdForm.generalParameterslow.forEach(function (Element, i) {
+                if (Element) {
+                    console.log("Element", Element);
+                    parameter.push({
+                        type: i,
+                        lowLimit: Element,
+                        highLimit: thresholdForm.generalParametershigh[i],
+                        parameterId: props.threshodId ? thresholdForm.udid[i] : "",
+                    });
+                }
+            });
+            if (props.threshodId != null) {
+                store.dispatch("updateGeneralParameterGroup", {
+                    data: {
+                        deviceTypeId: thresholdForm.deviceTypeId,
+                        generalParameterGroup: thresholdForm.generalParametersGroup,
+                        parameter: parameter,
+                    },
+                    id: props.threshodId,
+                }).then(() => {
+                    store.dispatch("generalParameterList")
+                    emit("is-visible", false)
+                    handleCancel()
+                })
+            } else {
+                store.dispatch("addGeneralParameterGroup", {
+                    deviceTypeId: thresholdForm.deviceTypeId,
+                    generalParameterGroup: thresholdForm.generalParametersGroup,
+                    parameter: parameter,
+                }).then(() => {
+                    store.dispatch("generalParameterList")
+                    emit("is-visible", false)
+                    handleCancel()
+                })
+            }
+
+        };
+
+        const handleCancel = () => {
+            formRef.value.resetFields();
+            Object.assign(thresholdForm, form);
+            store.commit("vitalNull");
+        };
+        watchEffect(() => {
+            if (props.threshodId != null) {
+                if (vitalEdit.value.vitalEdit) {
+                    store.commit("loadingStatus", true);
+                    store.dispatch("getVital", vitalEdit.value.vitalEdit.deviceTypeId);
+
+                    Object.assign(thresholdForm, vitalEdit.value.vitalEdit);
+
+                    setTimeout(() => {
+                        store.commit("loadingStatus", false);
+                    }, 2000);
+                }
+            }
+        });
+
+        return {
+            handleDevice,
+            globalCode,
+            thresholdForm,
+            vitalData,
+            handleCancel,
+            submitForm,
+            selectedItems,
+            filteredOptions,
+            filteredOptionsForTag,
+            selectedItemsForTag,
+            size: ref("large"),
+            value,
+            value2,
+            formRef,
+            regex,
+            vitalEdit,
+            closeModal,
+            formButton,
+        };
+    },
 };
 </script>
