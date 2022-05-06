4a24652d38de1b659cbddce070e96e9a1b25492b
harkeemat-bhullar <harkeemat.singh@ditstek.com>
2022-05-06 11:38:19 +0530

    create section module

---
+++ src/components/administration/template-section/QuestionTemplateDetail.vue
@@ -0,0 +1,260 @@
+<template>
+    <a-layout>
+      <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
+        <Header />
+      </a-layout-header>
+      <a-layout>
+        <Sidebar />
+        <a-layout-content>
+          <div class="common-bg">
+            <div class="questionnaireMain">
+              <a-row :gutter="24">
+                <a-col :span="24">
+                  <h2 class="pageTittle">
+                    Physician Orders for Life-Sustaining Treatment (POLST)
+                    <div class="commonBtn">
+                      <a-button class="btn secondaryBtn" @click="showModal2"
+                        >Search Question</a-button
+                      >
+                      <a-button class="btn secondaryBtn ml-10 buttonWtIcon" @click="showModal"
+                        >Add Question <PlusOutlined /></a-button
+                      >
+                      <router-link
+                        to="/questionnaire-template"
+                        class="b-inline ml-10"
+                      >
+                        <a-button class="btn primaryBtn">Back</a-button>
+                      </router-link>
+                    </div>
+                  </h2>
+                </a-col>
+                <a-col :span="24">
+                  <a-collapse
+                    v-model:activeKey="activeKey"
+                    expand-icon-position="left"
+                  >
+                    <a-collapse-panel
+                      key="1"
+                      header="Cardiopulmonary Resuscitation (CPR)"
+                    >
+                      <a-col :span="24">
+                        <a-checkbox
+                          class="questionOutput"
+                          v-model:checked="checked1"
+                        >
+                          <p>Attempt Resuscitation/CPR</p>
+                          <a-tag>RPM - 4</a-tag>
+                          <a-tag>TCM - 2</a-tag>
+                          <a-tag>BHI - 1</a-tag>
+                        </a-checkbox>
+                      </a-col>
+                      <a-col :span="24">
+                        <a-checkbox
+                          class="questionOutput"
+                          v-model:checked="checked2"
+                        >
+                          <p>Do Not Attempt Resuscitation/DNR</p>
+                          <a-tag>RPM - 4</a-tag>
+                          <a-tag>TCM - 2</a-tag>
+                        </a-checkbox>
+                      </a-col>
+                      <template #extra>
+                        <EditOutlined @click="showModal1" />
+                        <DeleteOutlined
+                      /></template>
+                    </a-collapse-panel>
+                    <a-collapse-panel
+                      key="2"
+                      header="Medical Interventions"
+                      :disabled="false"
+                    >
+                      <a-col :span="24">
+                        <a-checkbox
+                          class="questionOutput"
+                          v-model:checked="checked5"
+                        >
+                          <p>
+                            Full Treatment - primary goal of prolonging life by
+                            all medically effective means. In addition to
+                            treatment described in Selective Treatment and
+                            Comfort-Focused Treatment, use intubation, advanced
+                            airway interventions, mechanical ventilation, and
+                            cardioversion as indicated
+                          </p>
+                          <a-tag>RPM - 4</a-tag>
+                          <a-tag>TCM - 2</a-tag>
+                          <a-tag>BHI - 1</a-tag>
+                        </a-checkbox>
+                      </a-col>
+                      <a-col :span="24">
+                        <a-checkbox
+                          class="questionOutput"
+                          v-model:checked="checked6"
+                        >
+                          <p>
+                            Selective Treatment - goal of treating medical
+                            conditions while avoiding burdensome measures. In
+                            addition to treatment described in Comfort-Focused
+                            Treatment, use medical treatment, IV antibiotics, and
+                            IV fluids as indicated. Do not intubate. May use
+                            non-invasive positive airway pressure. Generally avoid
+                            intensive care
+                          </p>
+                          <a-tag>RPM - 4</a-tag>
+                          <a-tag>TCM - 2</a-tag>
+                        </a-checkbox>
+                      </a-col>
+                      <a-col :span="24">
+                        <a-checkbox
+                          class="questionOutput"
+                          v-model:checked="checked7"
+                        >
+                          <p>
+                            Selective Treatment - goal of treating medical
+                            conditions while avoiding burdensome measures. In
+                            addition to treatment described in Comfort-Focused
+                            Treatment, use medical treatment, IV antibiotics, and
+                            IV fluids as indicated. Do not intubate. May use
+                            non-invasive positive airway pressure. Generally avoid
+                            intensive care
+                          </p>
+                          <a-tag>TCM - 2</a-tag>
+                          <a-tag>BHI - 1</a-tag>
+                        </a-checkbox>
+                      </a-col>
+                      <template #extra>
+                        <EditOutlined @click="showModal1" />
+                        <DeleteOutlined
+                      /></template>
+                    </a-collapse-panel>
+                    <a-collapse-panel
+                      key="3"
+                      header="Artificially Administered Nutrition"
+                    >
+                      <a-col :span="24">
+                        <a-checkbox
+                          class="questionOutput"
+                          v-model:checked="checked9"
+                        >
+                          <p>
+                            Long-term artificial nutrition, including feeding
+                            tubes
+                          </p>
+                          <a-tag>RPM - 4</a-tag>
+                          <a-tag>TCM - 2</a-tag>
+                          <a-tag>BHI - 1</a-tag>
+                        </a-checkbox>
+                      </a-col>
+                      <a-col :span="24">
+                        <a-checkbox
+                          class="questionOutput"
+                          v-model:checked="checked10"
+                        >
+                          <p>
+                            Trial period of artificial nutrition, including
+                            feeding tubes
+                          </p>
+                          <a-tag>RPM - 4</a-tag>
+                          <a-tag>TCM - 2</a-tag>
+                        </a-checkbox>
+                      </a-col>
+                      <a-col :span="24">
+                        <a-checkbox
+                          class="questionOutput"
+                          v-model:checked="checked11"
+                        >
+                          <p>
+                            No artificial means of nutrition, including feeding
+                            tubes
+                          </p>
+                          <a-tag>TCM - 2</a-tag>
+                          <a-tag>BHI - 1</a-tag>
+                        </a-checkbox>
+                      </a-col>
+                      <a-col :span="24">
+                        <a-checkbox
+                          class="questionOutput last"
+                          v-model:checked="checked12"
+                        >
+                          <p>Obtain pharmacist consult</p>
+                          <a-tag>RPM - 4</a-tag>
+                          <a-tag>BHI - 1</a-tag>
+                        </a-checkbox>
+                      </a-col>
+                      <template #extra>
+                        <EditOutlined @click="showModal1" />
+                        <DeleteOutlined
+                      /></template>
+                    </a-collapse-panel>
+                  </a-collapse>
+                </a-col>
+              </a-row>
+            </div>
+            <!--modals-->
+            <AddQuestionnaire v-model:visible="visible2" @ok="handleOk" />
+            <EditQuestionnaire v-model:visible="visible3" @ok="handleOk" />
+            <SearchQuestion v-model:visible="visible1" @ok="handleOk" />
+            <!---->
+          </div>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </template>
+  <script>
+  import { defineComponent, ref } from "vue";
+  import Header from "@/components/administration/layout/header/Header";
+  import Sidebar from "@/components/administration/layout/sidebar/Sidebar";
+  import { DeleteOutlined, EditOutlined,PlusOutlined } from "@ant-design/icons-vue";
+  import AddQuestionnaire from "@/components/modals/AddQuestionnaire";
+  import EditQuestionnaire from "@/components/modals/EditQuestionnaire";
+  import SearchQuestion from "@/components/modals/SearchQuestion";
+  export default defineComponent({
+    components: {
+      Header,
+      Sidebar,
+      DeleteOutlined,
+      EditOutlined,
+      AddQuestionnaire,
+      EditQuestionnaire,
+      SearchQuestion,
+      PlusOutlined
+    },
+    setup() {
+      const activeKey = ref(["1"]);
+      const visible2 = ref(false);
+      const showModal = () => {
+        visible2.value = true;
+      };
+      const visible3 = ref(false);
+      const showModal1 = () => {
+        visible3.value = true;
+      };
+      const visible1 = ref(false);
+      const showModal2 = () => {
+        visible1.value = true;
+      };
+      return {
+        checked1: ref(false),
+        checked2: ref(false),
+        checked3: ref(false),
+        checked4: ref(false),
+        checked5: ref(false),
+        checked6: ref(false),
+        checked7: ref(false),
+        checked8: ref(false),
+        checked9: ref(false),
+        checked10: ref(false),
+        checked11: ref(false),
+        checked12: ref(false),
+        activeKey,
+        visible2,
+        showModal,
+        visible3,
+        showModal1,
+        visible1,
+        showModal2
+      };
+    },
+  });
+  </script>
+   
---
+++ src/components/administration/template-section/SectionTable.vue
@@ -0,0 +1,165 @@
+<template>
+<a-table rowKey="id" :columns="columns" :data-source="data" :scroll="{ x: 900 }" @change="handleTableChange" :pagination=false>
+    <template #question="text">
+        <router-link to="question-template-detail">{{
+                            text.text
+                          }}</router-link>
+    </template>
+    <template #actions="{record}">
+        <a-tooltip placement="bottom">
+            <template #title>
+                <span>Edit</span>
+            </template>
+            <a class="icons" @click="editModal(record.id)">
+                <EditOutlined /></a>
+        </a-tooltip>
+        <a-tooltip placement="bottom">
+            <template #title>
+                <span>Clone</span>
+            </template>
+            <a class="icons">
+                <CopyOutlined /></a>
+        </a-tooltip>
+        <a-tooltip placement="bottom">
+            <template #title>
+                <span>Delete</span>
+            </template>
+            <a class="icons" @click="deleteModal(record.id)">
+                <DeleteOutlined /></a>
+        </a-tooltip>
+    </template>
+    <template #active="key">
+        <a-switch v-model:checked="checked[key.record.key]" />
+    </template>
+</a-table>
+<Loader/>
+</template>
+<script>
+import {DeleteOutlined,EditOutlined,CopyOutlined,} from "@ant-design/icons-vue"
+import {useStore} from "vuex"
+import Loader from "@/components/loader/Loader"
+import {messages} from "@/config/messages";
+import {warningSwal} from "@/commonMethods/commonMethod";
+import { onMounted } from "vue"
+const columns = [{
+        title: "Questionnaire Template",
+        dataIndex: "templateName",
+
+        slots: {
+            customRender: "templateName",
+        },
+    },
+    {
+        title: "Type",
+        dataIndex: "templateType",
+    },
+    {
+        title: "Actions",
+        dataIndex: "actions",
+        slots: {
+            customRender: "actions",
+        },
+    },
+];
+
+export default {
+    components: {
+        DeleteOutlined,
+        EditOutlined,
+        CopyOutlined,
+        Loader,
+    },
+    props: {},
+    setup(props, {
+        emit
+    }) {
+        const store = useStore();
+        const editModal = (id) => {
+            store.dispatch('detailsQuestionnaireTemplate',id)
+            emit("edit",{show:true,id:id});
+        };
+        const data = store.getters.questionnaireTemplateList
+        const meta = store.getters.questionnaireTemplateMeta 
+        let record = []
+        let scroller = ""
+        onMounted(() => {
+            var tableContent = document.querySelector(".ant-table-body");
+            tableContent.addEventListener("scroll", (event) => {
+                let maxScroll = event.target.scrollHeight - event.target.clientHeight;
+                let currentScroll = event.target.scrollTop + 2
+                if (currentScroll >= maxScroll) {
+                    let current_page = meta.value.current_page + 1
+                    if (current_page <= meta.value.total_pages) {
+                        scroller = maxScroll;
+                        meta.value = ""
+                        record = data.value
+                        store.dispatch("questionnaireTemplateList", "?page=" + current_page + store.getters.searchTable.value +
+                            store.getters.orderTable.value.data).then(() => {
+                            loadMoredata()
+                        });
+                    }
+                }
+            });
+        });
+
+        function loadMoredata() {
+            const newData = data.value
+            newData.forEach((element) => {
+                record.push(element)
+            });
+            data.value = record;
+            var tableContent = document.querySelector(".ant-table-body");
+
+            setTimeout(() => {
+                tableContent.scrollTo(0, scroller);
+            }, 50)
+        }
+
+        function deleteModal(id){
+              warningSwal(messages.deleteWarning).then((response) => {
+                if (response == true) {
+                    var index = data.value.findIndex(function (o) {
+                        return o.id === id;
+                    })
+                   store.dispatch('deleteQuestionnaireTemplate',id)
+                    data.value.splice(index, 1)
+                }
+            })
+            
+        }
+         const handleTableChange = (pag, filters, sorter) => {
+            if (sorter.order) {
+                let order = sorter.order == "ascend" ? "ASC" : "DESC";
+                let orderParam = "&orderField=" + sorter.field + "&orderBy=" + order;
+                store.dispatch("orderTable", {
+                    data: orderParam,
+                    orderBy: order,
+                    page: pag,
+                    filters: filters,
+                });
+                store.dispatch(
+                    "questionnaireTemplateList",
+                    "?page=" + store.getters.searchTable.value + orderParam
+                );
+            } else {
+                store.dispatch("orderTable", {
+                    data: "&orderField=&orderBy=",
+                });
+                store.dispatch(
+                    "questionnaireTemplateList",
+                    "?page=" +
+                    store.getters.searchTable.value +
+                    store.getters.orderTable.value.data
+                )
+            }
+        }
+        return {
+            columns,
+            data,
+            editModal,
+            deleteModal,
+            handleTableChange,
+        };
+    },
+};
+</script>
